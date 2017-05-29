133b10f3d29e62f48cd5918748f52a59ebd92331
Jason Yu <me@ycmjason.com>
2017-05-29 12:13:23 +0100

    added filter for payment method

--- index.js
+++ index.js
@@ -4,27 +4,31 @@ const AMOUNT = 50;  // GBP
 //
 //
 //
+const NUMBER_OF_ROWS = process.argv[2] || 10;
+const output = process.argv[3] || "table";
+const ONLINE_PROVIDER = ['NATIONAL_BANK', 'PAYPAL', 'GIFT_CARD_CODE_AMAZON', 'CASH_BY_MAIL', 'CASH_DEPOSIT'][0];
+
 var fetch = require('node-fetch');
 var _uniqWith = require('lodash.uniqwith');
 var _isEqual = require('lodash.isequal');
 var Table = require('cli-table');
 var colors = require('colors');
 
-const NUMBER_OF_ROWS = process.argv[2] || 10;
-const output = process.argv[3] || "table";
-
 const roundTo = (x, p) => {
   var factor = Math.pow(10, p)
   return Math.round(x * factor) / factor
 }
 
-var btc_amount = roundTo(AMOUNT / BUY_IN_RATE, 8);
+const roundBTC = (x) => roundTo(x, 8);
+
+var btc_amount = roundBTC(AMOUNT / BUY_IN_RATE);
 
 var promise = fetch('https://localbitcoins.com/sell-bitcoins-online/GBP/.json')
   .then(res => res.json())
   .then(res => res.data)
   .then(data => data.ad_list.map(ad => ad.data))
-  .then(data => _uniqWith(data.map(_parseSale), _isEqual))
+  .then(sales => sales.filter(sale => sale.online_provider === ONLINE_PROVIDER))
+  .then(sales => _uniqWith(sales.map(_parseSale), _isEqual))
   .then(analyseSales)
   .then(sales => sales.slice(0, NUMBER_OF_ROWS))
   .then(sales => sales.map((sale, i) => Object.assign({id: i + 1}, sale)))
@@ -34,8 +38,8 @@ function analyseSales(sales){
   return sales.map(sale => {
     var gross = sale.price * btc_amount;
     return Object.assign(sale, {
-      gross: sale.price * btc_amount,
-      net: gross - AMOUNT,
+      gross: roundBTC(sale.price * btc_amount),
+      net: roundBTC(gross - AMOUNT),
     });
   });
 }
@@ -47,7 +51,13 @@ function showTable(sales){
   });
   sales.map((sale) => {
     var color = (x) => colors[(sale.net > 0)? 'green': 'red'](x);
-    table.push([sale.id, sale.seller, sale.price, sale.gross, sale.net].map(color));
+    table.push([
+      sale.id,
+      sale.seller,
+      sale.price,
+      sale.gross,
+      sale.net
+    ].map(color));
   });
   console.log(table.toString());
 }
