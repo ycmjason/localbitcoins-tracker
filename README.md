a85fb1d91f00a2cfb42f621eee90b256e35e77e6
Jason Yu <me@ycmjason.com>
2017-05-29 02:35:29 +0100

    added json output format

--- README.md
+++ README.md
@@ -53,6 +53,28 @@ You can also list more rows (max 30 rows) if you want.
 ... 30 rows in total ...
 ```
 
+It might be useful tho if there is JSON format output for further analysis.
+```bash
+> npm run 10 json
+[
+    {
+        "id": 1,
+        "seller": "footnote (24; 100%)",
+        "price": "1800.00",
+        "gross": 47.787696000000004,
+        "net": -2.212303999999996
+    },
+    {
+        "id": 2,
+        "seller": "rustyspatula (100+; 100%)",
+        "price": "1785.87",
+        "gross": 47.4125625864,
+        "net": -2.5874374136
+    },
+    ... 10 records in total ...
+```
+
+
 ### Run it every hour
 You can configure a cron task to make the script run every hour.
 For example:
@@ -64,7 +86,7 @@ For example:
 Then in the editor, add a line:
 
 ```
-25 * * * * node $HOME/bitcoin-tracker/index.js >> $HOME/logs/`date +'\%Y-\%b-\%d'`.log 2>&1
+25 * * * * node $HOME/bitcoin-tracker/index.js 10 json>> $HOME/logs/`date +'\%Y-\%b-\%d'`.log 2>&1
 ```
 
 The above script would create a file every day containing all the logs of the day.
--- index.js
+++ index.js
@@ -10,7 +10,8 @@ var _isEqual = require('lodash.isequal');
 var Table = require('cli-table');
 var colors = require('colors');
 
-const NUMBER_OF_ROWS = 10 || process.argv[2];
+const NUMBER_OF_ROWS = process.argv[2] || 10;
+const output = process.argv[3] || "table";
 
 const roundTo = (x, p) => {
   var factor = Math.pow(10, p)
@@ -24,21 +25,35 @@ var promise = fetch('https://localbitcoins.com/sell-bitcoins-online/GBP/.json')
   .then(res => res.data)
   .then(data => data.ad_list.map(ad => ad.data))
   .then(data => _uniqWith(data.map(_parseSale), _isEqual))
-  .then(show);
+  .then(analyseSales)
+  .then(sales => sales.slice(0, NUMBER_OF_ROWS))
+  .then(sales => sales.map((sale, i) => Object.assign({id: i + 1}, sale)))
+  .then((output === "json")? showJson: showTable);
 
-function show(sales){
+function analyseSales(sales){
+  return sales.map(sale => {
+    var gross = sale.price * btc_amount;
+    return Object.assign(sale, {
+      gross: sale.price * btc_amount,
+      net: gross - AMOUNT,
+    });
+  });
+}
+
+function showTable(sales){
   var table = new Table({
     head: ['#', 'Name', 'Price (GBP/BTC)', 'How much you get (GBP)', 'How much you earn (GBP)'],
     style: {head: ['bold']}
   });
-  sales = sales.slice(0, NUMBER_OF_ROWS);
-  sales.map((sale, i) => {
-    var gbp_get = sale.price * btc_amount;
-    var gbp_earn = gbp_get - AMOUNT;
-    gbp_earn = colors[(gbp_earn > 0)? 'green': 'red'](gbp_earn);
-    table.push([i + 1, sale.seller, sale.price, gbp_get, gbp_earn]);
+  sales.map((sale) => {
+    var color = (x) => colors[(sale.net > 0)? 'green': 'red'](x);
+    table.push([sale.id, sale.seller, sale.price, sale.gross, sale.net].map(color));
   });
   console.log(table.toString());
+}
+
+function showJson(sales){
+  console.log(JSON.stringify(sales, null, 4));
 }
 
 function _parseSale(sale){
